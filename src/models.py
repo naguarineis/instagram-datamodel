606d0e8e6874d2bfe723862ec2c338591fb69171
David Chacón <davidchacon1993@gmail.com>
2022-02-11 02:59:38 +0000

    First commit: created Ig database model and diagrm.png

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,82 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
+
+class Followers(Base):
+    __tablename__ = 'followers'
+    follower_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    followed_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(50), nullable=False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    about = Column(String(250), nullable=False)
+    phone = Column(Integer, nullable=False)
+    email = Column(String(50), nullable=False)
+
+    post = relationship("Post", back_populates="parent")
+    comment = relationship("Comment", back_populates="parent")
+    followed = relationship("User",
+        secondary = Followers,
+        primaryjoin = (Followers.follower_id == id),
+        secondaryjoin = (Followers.followed_id == id),
+        backref = relationship("Followers", backref="parent"),
+        lazy = 'dynamic')
+    
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    link = Column(String(250), nullable=False)
+    description = Column(Text, nullable=False)
+    user_id= Column(Integer, ForeignKey("user.id"))
+
+    comment = relationship("Comment", back_populates="parent")
+    tag = relationship("Tag", back_populates="parent")
+
+    user = relationship("User", back_populates="children")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    created_at = Column(DateTime)
+
+    user = relationship("User", back_populates="children")
+    post = relationship("Post", back_populates="children")
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    description = Column(String(250), nullable=False) 
+
+    post = relationship("Post", back_populates="children")
 
     def to_dict(self):
         return {}
